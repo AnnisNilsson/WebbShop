48d3cd6c16fa58f1f29f2e72d463c06a012bce53
josiehpersson <josephine.h.persson@outlook.com>
2020-09-11 09:27:29 +0200

    Update customerRouter.js

--- API/Routes/customerRouter.js
+++ API/Routes/customerRouter.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const customersController = require('../controllers/customersController');
+const customersController = require('../Controllers/customersController');
 
 const routes = () => {
-const customerRouter = express.Router();
-const controller = customersController();
-customerRouter.route('/Customer')
- .get(controller.get);
-customerRouter.route('/Customer/:Id')
- .get(controller.get);
-return customerRouter;
+    const customerRouter = express.Router();
+    const controller = customersController();
+    customerRouter.route('/Customers')
+    .get(controller.get);
+    customerRouter.route('/Customer/:Id')
+    .get(controller.get);
+    console.log(controller.result);
+    return customerRouter;
 };
 
 
